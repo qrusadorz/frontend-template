fdb41a884b8de2389193ed24850eacf383108262
qrusadorz <qrusadorz@gmail.com>
2021-01-09 23:17:46 +0900

    merge PWA

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@material-ui/core/styles';
 // import './index.css'; // cra default
 import App from './App';
 import theme from './theme';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -23,6 +24,11 @@ ReactDOM.render(
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
